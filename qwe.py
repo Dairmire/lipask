1e5512d00c1c1b6829114cea952b41623396f3ff
Dairmire <sidorenkovnike@gmail.com>
2024-10-06 00:29:37 +0300

    aaaaaaaaaaaaa

--- qwe.py
+++ qwe.py
@@ -5,25 +5,15 @@ def main():
     data = a().splitlines()
     n, q = map(int, data[0].split())
     arrows = list(map(int, data[1].split()))
-    k = [int(data[i + 2]) for i in range(q)]
-    results = []
-    for k in k:
-        valid_arrows = [arrow for arrow in arrows if arrow > k]
-        if not valid_arrows:
-            results.append(0)
-            continue
-        m = min(valid_arrows)
-        robin_ar = m + 1
-        if robin_ar > 2 * m:
-            results.append(0)
-        else:
-            results.append(robin_ar)
+    k = list(map(int, data[2::]))
+    for i in k:
 
-    for result in results:
-        print(result)
+        if (max(arrows) + 1) // 2 > i:
+            print('0')
+        else:
+            print(max(arrows) + 1)
 
 
 if __name__ == "__main__":
     main()
 
-ййййййййййййй
